e5d34bc92373f5872cbad94abed133a9734b41b0
Dionathan Chrys <98222996+dionathanchrys@users.noreply.github.com>
2024-01-09 23:43:08 -0300

    fix: adjusts multi cluster (#5)
    * fix: some adjusts, tested services and development environments
    
    * fix: adjusts labels and bash script
    
    * add: kubeclt wait (Closes #1 )
    
    * fix: bash script, hosts example
    
    * swift to ip
    
    * fix: controller hpa

--- app/kind-cluster/base/create-envs.sh
+++ app/kind-cluster/base/create-envs.sh
@@ -1,21 +1,73 @@
-#!/bin/zsh
-echo "#### SVC CLUSTER ####" && echo " "
+#!/bin/bash
+echo " " && echo "➡ ➡ ➡ ➡ CREATE DOCKER NETWORK ⬅ ⬅ ⬅ ⬅" && echo " "
+docker network create cluster-kind -d bridge --subnet 192.168.222.0/24
+
+echo " " && echo "➡ ➡ ➡ ➡ CREATING SVC CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 kind create cluster --config app/kind-cluster/base/cluster-svc.yaml
-docker network create svc-env -d bridge --subnet 192.168.222.0/30
 docker network disconnect kind svc-cluster-control-plane
-docker network connect svc-env svc-cluster-control-plane
+docker network connect cluster-kind svc-cluster-control-plane --ip 192.168.222.10
+echo " " && echo "➡ ➡ ➡ ➡ RESTARTING SVC CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 docker restart svc-cluster-control-plane
 
-echo "#### DEV CLUSTER ####" && echo " "
+echo " " && echo "➡ ➡ ➡ ➡ CREATING DEV CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 kind create cluster --config app/kind-cluster/base/cluster-dev.yaml
-docker network create dev-env -d bridge --subnet 192.168.222.4/30
 docker network disconnect kind dev-cluster-control-plane
-docker network connect dev-env dev-cluster-control-plane
+docker network connect cluster-kind dev-cluster-control-plane --ip 192.168.222.20
+echo " " && echo "➡ ➡ ➡ ➡ RESTARTING DEV CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 docker restart dev-cluster-control-plane
 
-echo "#### PRD CLUSTER ####" && echo " "
+echo " " && echo "➡ ➡ ➡ ➡ CREATING PRD CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 kind create cluster --config app/kind-cluster/base/cluster-prd.yaml
-docker network create prd-env -d bridge --subnet 192.168.222.8/30
 docker network disconnect kind prd-cluster-control-plane
-docker network connect prd-env prd-cluster-control-plane
+docker network connect cluster-kind prd-cluster-control-plane --ip 192.168.222.30
+echo " " && echo "➡ ➡ ➡ ➡ RESTARTING PRD CLUSTER ⬅ ⬅ ⬅ ⬅" && echo " "
 docker restart prd-cluster-control-plane
+
+############################################################################
+
+echo " " && echo "➡ ➡ ➡ ➡ CREATING SVC NAMESPACES ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/kind-cluster/overlay/service --context kind-svc-cluster
+
+echo " " && echo "➡ ➡ ➡ ➡ CREATING DEV NAMESPACES ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/kind-cluster/overlay/dev --context kind-dev-cluster
+
+echo " " && echo "➡ ➡ ➡ ➡ CREATING PRD NAMESPACES ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/kind-cluster/overlay/prd --context kind-prd-cluster
+
+############################################################################
+
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING BOUNDARY DATABASE ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/boundary-postgres/overlay/service --context kind-svc-cluster
+echo " " && echo "➡ ➡ ➡ ➡ WAITING BOUNDARY DATABASE ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl wait -f app/boundary-postgres/base/deployment.yaml --for condition=Available --timeout 120s -n boundary --context kind-svc-cluster
+
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING DATABASE MIGRATOR ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/boundary-migrator/overlay/service --context kind-svc-cluster
+echo " " && echo "➡ ➡ ➡ ➡ WAITING DATABASE MIGRATOR ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl wait -f app/boundary-migrator/base/job.yaml --for condition=Complete --timeout 120s -n boundary --context kind-svc-cluster
+echo " " && echo "➡ ➡ ➡ ➡ BOUNDARY ADMIN CREDENTIALS ⬅ ⬅ ⬅ ⬅" && echo " "
+echo "####################################################"
+echo "####                                            ####"
+echo "####   Login:              admin                ####"
+echo "####                                            ####"
+echo "#### $(kubectl logs jobs/boundary-migrator -n boundary --context kind-svc-cluster | grep "Password:") ####"
+echo "####                                            ####"
+echo "####                                            ####"
+echo "####################################################"
+echo "  Login:              admin" > boundary-credential.txt
+echo "$(kubectl logs jobs/boundary-migrator -n boundary --context kind-svc-cluster | grep "Password:")" >> boundary-credential.txt && echo "Boundary credentials saved to boundary-credential.txt"
+
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING BOUNDARY CONTROLLER ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/boundary-controller/overlay/service --context kind-svc-cluster
+echo " " && echo "➡ ➡ ➡ ➡ WAITING BOUNDARY CONTROLLER ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl wait -f app/boundary-controller/base/deployment.yaml --for condition=Available --timeout 120s -n boundary --context kind-svc-cluster
+
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING BOUNDARY WORKER- DEV ENV ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/boundary-worker/overlay/dev --context kind-dev-cluster
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING DEV DATABASE ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/foo-postgres/overlay/dev --context kind-dev-cluster
+
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING BOUNDARY WORKER- PRD ENV ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/boundary-worker/overlay/prd --context kind-prd-cluster
+echo " " && echo "➡ ➡ ➡ ➡ DEPLOYING PRD DATABASE ⬅ ⬅ ⬅ ⬅" && echo " "
+kubectl apply -k app/foo-postgres/overlay/prd --context kind-prd-cluster
